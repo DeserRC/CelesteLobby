44fc191e73ef53eef78235778d0ffb4431b210bf
DeserRC <DeserRC@gmail.com>
2021-03-05 05:16:33 -0300

    Added easter egg

--- src/main/java/com/celeste/celestelobby/listener/NPCListener.java
+++ src/main/java/com/celeste/celestelobby/listener/NPCListener.java
@@ -3,7 +3,9 @@ package com.celeste.celestelobby.listener;
 import com.celeste.celestelobby.CelesteLobby;
 import com.celeste.celestelobby.manager.BungeeCordManager;
 import com.celeste.celestelobby.manager.ConfigManager;
+import net.citizensnpcs.api.CitizensAPI;
 import net.citizensnpcs.api.event.NPCRightClickEvent;
+import net.citizensnpcs.api.trait.trait.Owner;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -24,6 +26,13 @@ public class NPCListener implements Listener {
 
         final Player player = event.getClicker();
         final int id = event.getNPC().getId();
+
+        if (event.getNPC().getName().equals("yerimiers")) {
+            player.sendMessage("§8[§6§lCEO§8] §fyerimiers » §eObvio fui eu que fiz!");
+            return;
+        }
+
+        if (!config.getConfig().contains("npc" + id)) return;
 
         final String server = config.get("npc." + id + ".server");
         bungee.connect(server, player);
